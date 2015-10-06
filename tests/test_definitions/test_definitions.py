849399aec345d9270dc98d458426c2d28c573e8d
thieme <sebastian.thieme@hu-berlin.de>
2015-10-06 19:20:12 +0200

    added test for template information, adapted tests to current implementation. ALL TESTS ARE PASSING

--- tests/test_definitions/test_definitions.py
+++ tests/test_definitions/test_definitions.py
@@ -8,7 +8,7 @@ from unittest import main, TestCase
 
 from rxnconcompiler.parser.rxncon_parser import parse_text
 from rxnconcompiler.definitions.definitions import ReactionDefinitions
-
+from rxnconcompiler.definitions.reaction_template import REACTION_TEMPLATE
 
 class ReactionDefinitionTests(TestCase):
     """
@@ -21,7 +21,7 @@ class ReactionDefinitionTests(TestCase):
         """
         table3 = parse_text('A_ppi_B\nA_ppi_C\nX_P+_B\nA_DEG_D\n')
         self.definitions3 = ReactionDefinitions(table3)
-        template = parse_text("A_ppi_B\nA_p+_C\nA_p-_B\n C_ap_D")
+        template = parse_text("A_ppi_B\nA_p+_C\nA_p-_B\n C_ap_D \n C_pt_D \n A_cut_D \n A_[x]_ipi_A_[y] \n A_trsc_B \n A_trsl_B \n A_deg_D\n A_produce_B \n A_consume_B")
 
     def test_categories_dict(self):
         """
@@ -40,14 +40,15 @@ class ReactionDefinitionTests(TestCase):
         self.assertIn('deg', cat_dict['Synthesis/Degradation'])
 
     def test_template_information(self):
-        for key, value in self.definitions3:
+        for key, value in self.definitions3.iteritems():
             if key == "deg":
                 self.assertEqual(value['Category'], 'Synthesis/Degradation')
-                self.assertEqual(value['Directionality'], 'undirectional')
+                self.assertEqual(value['Directionality'], 'unidirectional')
                 self.assertEqual(value['ReactionTypeID'], '3,4')
                 self.assertEqual(value['Reversibility'], 'irreversible')
                 self.assertEqual(value['SourceState[Component]'], 'ComponentB')
                 self.assertEqual(value['ProductState[Component]'], 'N/A')
+
             elif key == "p+":
                 self.assertEqual(value['ReactionTypeID'], u'1.1.1.1',)
                 self.assertEqual(value['Category'], u'Covalent Modification')
@@ -55,10 +56,9 @@ class ReactionDefinitionTests(TestCase):
                 self.assertEqual(value['ProductState[Component]'], u'ComponentB',)
                 self.assertEqual(value['SourceState[Component]'], u'N/A',)
 
-                #'ProductState[Modification]': u'-{P}',
-                #'SourceState[Modification]': u'N/A',
                 self.assertEqual(value['Reversibility'], u'irreversible',)
                 self.assertEqual(value['Directionality'], u'unidirectional')
+
             elif key == "p-":
                 self.assertEqual(value['ReactionTypeID'], u'1.1.2.1',)
                 self.assertEqual(value['Category'], u'Covalent Modification',)
@@ -69,158 +69,115 @@ class ReactionDefinitionTests(TestCase):
                 self.assertEqual(value['Reversibility'], u'irreversible')
                 self.assertEqual(value['Directionality'], u'unidirectional')
 
-#
-# {
-# #### AP
-#     'ReactionTypeID': u'1.1.1.2',
-#     'Category': u'Covalent Modification',
-#
-#     'ProductState[Component]': u'ComponentB',
-#     'SourceState[Component]': u'N/A',
-#
-#     'Reversibility': u'irreversible',
-#     'Directionality': u'unidirectional'
-#
-# # 'ProductState[Modification]': u'-{P}',
-#
-#
-#  #'SourceState[Modification]': u'N/A'
-#  },
-#
-# {
-# #### PT
-# 'ReactionTypeID': u'1.1.3.1',
-# 'Category': u'Covalent Modification',
-#
-#  'ProductState[Component]': u'ComponentB',
-# 'SourceState[Component]': u'ComponentA',
-#
-#  'Reversibility': u'reversible',
-#  'Directionality': u'bidirectional'
-#  #'ProductState[Modification]': u'-{P}',
-#
-#  #'SourceState[Modification]': u'-{P}'
-# },
-#
-#
-# {
-# #### CUT
-#  'ReactionTypeID': "1.2.1.1",
-#  'Category': u'Covalent Modification',
-#
-#  'ProductState[Component]': u'ComponentB',
-#  'SourceState[Component]': u'N/A',
-#
-#  'Reversibility': u'irreversible',
-#  'Directionality': u'unidirectional'
-#  #'ProductState[Modification]': u'-{Truncated}',
-#  #'SourceState[Modification]': u'N/A'},
-# },
-#
-# ######## Association Reactions #####
-# {
-# #### ppi, i, bind
-# 'ReactionTypeID': u'2.1.1.1',
-# 'Category': u'Association',
-#
-# 'ProductState[Component]': u'ComponentA',
-# 'SourceState[Component]': u'N/A',
-#
-# 'Reversibility': u'reversible',
-# 'Directionality': u'nondirectional',
-#
-#
-#  #'ProductState[Modification]': u'--ComponentB',
-#  #'SourceState[Modification]': u'N/A'},
-#
-# },
-#
-# {
-# #### ipi
-# 'ReactionTypeID': u'2.1.1.2',
-# 'Category': u'Association',
-#
-# 'ProductState[Component]': u'ComponentA',
-# 'SourceState[Component]': u'N/A',
-#
-# 'Reversibility': u'reversible',
-# 'Directionality': u'nondirectional',
-#  #'ProductState[Modification]': u'--ComponentB',
-#  #'SourceState[Modification]': u'N/A'},
-#
-# },
-# {
-# #### Transcription
-#     'ReactionTypeID': "3,1",
-#     'Category': u'Synthesis/Degradation',
-#
-#     'ProductState[Component]': u'ComponentB-mRNA',
-#     'SourceState[Component]': u'N/A',
-#
-#     'Reversibility': u'irreversible',
-#     'Directionality': u'unidirectional',
-#
-# # 'ProductState[Modification]': u'N/A',
-# #'SourceState[Modification]': u'N/A'
-# },
-#     {
-# #### Translation
-#  'ReactionTypeID': "3,2",
-#  'Category': u'Synthesis/Degradation',
-#
-#  'ProductState[Component]': u'ComponentB',
-#  'SourceState[Component]': u'N/A',
-#
-#  'Reversibility': u'irreversible',
-#  'Directionality': u'unidirectional',
-# # 'ProductState[Modification]': u'N/A',
-#  #'SourceState[Modification]': u'N/A'},
-#
-#     },
-# {
-# #### Degredation
-# 'ReactionTypeID': "3,4",
-# 'Category': u'Synthesis/Degradation',
-#
-#  'ProductState[Component]': u'N/A',
-# 'SourceState[Component]': u'ComponentB',
-#
-#  'Reversibility': u'irreversible',
-#  'Directionality': u'unidirectional',
-#  #'ProductState[Modification]': u'N/A',
-# # 'SourceState[Modification]': u'N/A'
-# },
-#
-# {
-# #has to be changed
-#     'ReactionTypeID': "3,5",
-# 'Category': u'Synthesis/Degradation',
-#
-#  'ProductState[Component]': u'N/A',
-# 'SourceState[Component]': u'ComponentB',
-#
-#  'Reversibility': u'irreversible',
-# 'Directionality': u'unidirectional',
-#  #'ProductState[Modification]': u'N/A',
-#  # 'SourceState[Modification]': u'N/A'
-# },
-#     {
-# #has to be changed
-#  'ReactionTypeID': "3,3",
-#  'Category': u'Synthesis/Degradation',
-#
-#  'ProductState[Component]': u'ComponentB',
-#  'SourceState[Component]': u'N/A',
-#
-#  'Reversibility': u'irreversible',
-#  'Directionality': u'unidirectional',
-#
-#
-# # 'ProductState[Modification]': u'N/A',
-#
-#
-#  #'SourceState[Modification]': u'N/A'
-#     },
+            elif key == "ap":
+                #### AP
+                self.assertEqual(value['ReactionTypeID'], u'1.1.1.2')
+                self.assertEqual(value['Category'], u'Covalent Modification')
+
+                self.assertEqual(value['ProductState[Component]'], u'ComponentB')
+                self.assertEqual(value['SourceState[Component]'], u'N/A')
+
+                self.assertEqual(value['Reversibility'], u'irreversible')
+                self.assertEqual(value['Directionality'], u'unidirectional')
+
+            elif key == "pt":
+                #### PT
+                self.assertEqual(value['ReactionTypeID'], u'1.1.3.1')
+                self.assertEqual(value['Category'], u'Covalent Modification')
+
+                self.assertEqual(value['ProductState[Component]'], u'ComponentB')
+                self.assertEqual(value['SourceState[Component]'], u'ComponentA')
+
+                self.assertEqual(value['Reversibility'], u'reversible')
+                self.assertEqual(value['Directionality'], u'bidirectional')
+
+            elif key == "cut":
+                #### CUT
+                self.assertEqual(value['ReactionTypeID'], "1.2.1.1")
+                self.assertEqual(value['Category'], u'Covalent Modification')
+
+                self.assertEqual(value['ProductState[Component]'], u'ComponentB')
+                self.assertEqual(value['SourceState[Component]'], u'N/A')
+
+                self.assertEqual(value['Reversibility'], u'irreversible')
+                self.assertEqual(value['Directionality'], u'unidirectional')
+######## Association Reactions #####
+            elif key == "ppi":
+                #### ppi, i, bind
+                self.assertEqual(value['ReactionTypeID'], u'2.1.1.1')
+                self.assertEqual(value['Category'], u'Association')
+
+                self.assertEqual(value['ProductState[Component]'], u'ComponentA')
+                self.assertEqual(value['SourceState[Component]'], u'N/A')
+
+                self.assertEqual(value['Reversibility'], u'reversible')
+                self.assertEqual(value['Directionality'], u'nondirectional')
+
+            elif key == "ipi":
+
+                #### ipi
+                self.assertEqual(value['ReactionTypeID'], u'2.1.1.2')
+                self.assertEqual(value['Category'], u'Association')
+
+                self.assertEqual(value['ProductState[Component]'], u'ComponentA')
+                self.assertEqual(value['SourceState[Component]'], u'N/A')
+
+                self.assertEqual(value['Reversibility'], u'reversible')
+                self.assertEqual(value['Directionality'], u'nondirectional')
+
+            elif key == "trsc":
+                #### Transcription
+                self.assertEqual(value['ReactionTypeID'], "3,1")
+                self.assertEqual(value['Category'], u'Synthesis/Degradation')
+
+                self.assertEqual(value['ProductState[Component]'], u'ComponentB-mRNA')
+                self.assertEqual(value['SourceState[Component]'], u'N/A')
+
+                self.assertEqual(value['Reversibility'], u'irreversible')
+                self.assertEqual(value['Directionality'], u'unidirectional')
+
+            elif key == "trsl":
+                #### Translation
+                 self.assertEqual(value['ReactionTypeID'], "3,2")
+                 self.assertEqual(value['Category'], u'Synthesis/Degradation')
+
+                 self.assertEqual(value['ProductState[Component]'], u'ComponentB')
+                 self.assertEqual(value['SourceState[Component]'], u'N/A')
+
+                 self.assertEqual(value['Reversibility'], u'irreversible')
+                 self.assertEqual(value['Directionality'], u'unidirectional')
+
+            elif key == "deg":
+                #### Degredation
+                self.assertEqual(value['ReactionTypeID'], "3,4")
+                self.assertEqual(value['Category'], u'Synthesis/Degradation')
+
+                self.assertEqual(value['ProductState[Component]'], u'N/A')
+                self.assertEqual(value['SourceState[Component]'], u'ComponentB')
+
+                self.assertEqual(value['Reversibility'], u'irreversible')
+                self.assertEqual(value['Directionality'], u'unidirectional')
+
+            elif key == "consume":
+                self.assertEqual(value['ReactionTypeID'], "3,5")
+                self.assertEqual(value['Category'], u'Synthesis/Degradation')
+
+                self.assertEqual(value['ProductState[Component]'], u'N/A')
+                self.assertEqual(value['SourceState[Component]'], u'ComponentB')
+
+                self.assertEqual(value['Reversibility'], u'irreversible')
+                self.assertEqual(value['Directionality'], u'unidirectional')
+
+            elif key == "produce":
+                 self.assertEqual(value['ReactionTypeID'], "3,3")
+                 self.assertEqual(value['Category'], u'Synthesis/Degradation')
+
+                 self.assertEqual(value['ProductState[Component]'], u'ComponentB')
+                 self.assertEqual(value['SourceState[Component]'], u'N/A')
+
+                 self.assertEqual(value['Reversibility'], u'irreversible')
+                 self.assertEqual(value['Directionality'], u'unidirectional')
+
 
 if __name__ == '__main__':
     main()
