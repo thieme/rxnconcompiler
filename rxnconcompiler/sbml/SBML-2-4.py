0d497ffc0799a102f7ebe33b9aa6e7440197b2bf
Robin <helliro@zedat.fu-berlin.de>
2015-12-15 11:31:11 +0100

    SBMLDocument() works, blank document&modell created SBML version 2.4 is default

--- rxnconcompiler/sbml/SBML-2-4.py
+++ rxnconcompiler/sbml/SBML-2-4.py
@@ -1,17 +1,26 @@
 from rxnconcompiler.SBGN.PD import ReducedProcessDescription
 from rxnconcompiler.rxncon import Rxncon
-from libsbml import *  #TODO imported all the libSBML, maybe specifie what is realy needed in the end
+from libsbml import * # TODO imported all the libSBML, maybe specifie what is realy needed in the end
 
 class SBMLBuilder(object):
 
-    def build_model(self, ReducedPDTree):
-
+    def __init__(self, level = 2, version = 4):
+        # creates a SBML document of given level and version, default is 2.4 because of Celldesigner specs
         try:
-            document = SBMLDocument()
+            self.document = SBMLDocument(level, version)
+            print("\nDocument created")
         except ValueError:
-            raise SystemExit('SBML Document creation faile') #TODO another exception handle requiered?
-        model = document.createModel()
-        model.setTimeUnits("second")
+            print("\nerror in init of sbml doc")
+            raise SystemExit("SBML Document creation failed")    # TODO another exception handle requiered?
+
+
+    def build_model(self, rPDTree):
+        # build_model
+
+        print("\nmodel creating")
+        model = self.document.createModel()
+        #model.setTimeUnits("second")
+        return(self.document)
 
 if __name__ == "__main__":
     TOY1 = """
@@ -22,5 +31,8 @@ if __name__ == "__main__":
     rxncon.run_process()
     reducedPD = ReducedProcessDescription(rxncon.reaction_pool)
     reducedPD.build_reaction_Tree()
+    #sb = SBMLBuilder(level = 3, version = 1)
     sb = SBMLBuilder()
     toy1sbml =  sb.build_model(reducedPD.tree)
+    print("\n" + writeSBMLToString(toy1sbml) + "\n")
+    #print(toy1sbml.model.getTimeUnits)
