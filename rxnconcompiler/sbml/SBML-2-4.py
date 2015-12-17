41a9f97d99924646f8ab062e1c93ed0f5db1637a
Robin <helliro@zedat.fu-berlin.de>
2015-12-17 10:55:56 +0100

    save model to sbml file at given path reduced set species attribute to the mandatory, no mor set arbitrary , compartment still one default

--- rxnconcompiler/sbml/SBML-2-4.py
+++ rxnconcompiler/sbml/SBML-2-4.py
@@ -1,5 +1,6 @@
 from rxnconcompiler.SBGN.PD import ReducedProcessDescription
 from rxnconcompiler.rxncon import Rxncon
+import os
 from libsbml import * # TODO imported all the libSBML, maybe specifie what is realy needed in the end
 # libsbml import always marked as unused and the depending methods as unresolved but work as intended when run
 
@@ -10,7 +11,7 @@ class SBMLBuilder(object):
         # creates a SBML document of given level and version, default is 2.4 because of Celldesigner specs
         try:
             self.document = SBMLDocument(level, version)
-            print("\nDocument created")
+            #print("\nDocument created")
         except ValueError:
             print("\nerror in init of sbml doc")
             raise SystemExit("SBML Document creation failed")    # TODO another exception handle requiered?
@@ -19,21 +20,26 @@ class SBMLBuilder(object):
     def process_node(self, visitId):
         #takes an unvisited node and creates species out of it
         #TODO only minimal necessary information are set, mostly defaults
-        #print(visitId)
+        #optional values are commented commented out until a rxncon variable is found
 
         species = self.model.createSpecies()
         for node in self.tree.nodes:
             if node.id == visitId:
-                species.setId("s" + str(node.id) )          #TODO SBML prefers ID with meaning so may be changed and become dependend on the name!? cant be just numerical
-                species.setName(str(node.name))
-                species.setCompartment('c1')                #TODO Compartment is static set to default comp c1
-                species.setConstant(False)                  #TODO find coresponding value in rxncon
-                species.setInitialAmount(node.id*2)         #TODO find coresponding value in rxncon actual value just for identification
-                species.setSubstanceUnits('mole')           #TODO find coresponding value in rxncon
-                species.setBoundaryCondition(False)         #TODO find coresponding value in rxncon
-                species.setHasOnlySubstanceUnits(False)     #TODO find coresponding value in rxncon
 
-        # TODO handle both nodes: already visited? if not insert the species
+                species.setId("s" + str(node.id) )
+                species.setName(str(node.name))
+                # optional set species.setSpeciesType()               #optional attribute
+                species.setCompartment('c1')                #TODO Compartment is set to default comp c1, find alternative
+                #species.setInitialAmount(
+                #species.setInitialConcentration()
+                #species.setSubstanceUnits('mole')
+                #species.setHasOnlySubstanceUnits(False)
+                #species.setBoundaryCondition(False)
+                #species.setConstant(False)
+
+
+
+
 
     def build_model(self, rPDTree):
         # build_model takes a reducedPD.tree
@@ -53,7 +59,7 @@ class SBMLBuilder(object):
         # the same will happen for each edge their reaction
         # TODO handle the edge itself with reaction etc.
         for edge in rPDTree.edges:
-            print(edge.id)
+            #print(edge.id)
             if edge.id[0] not in visited_nodes :
                 self.process_node(edge.id[0])
                 visited_nodes.append(edge.id[0])
@@ -63,6 +69,14 @@ class SBMLBuilder(object):
 
 
         return(self.document)
+
+    def save_SBML(self, document, path):
+        #TODO validation of model before writing to file!?
+        if writeSBMLToFile(document, path):
+            print("print  xuscessful")
+        else:
+            print('file failed')
+
 
 if __name__ == "__main__":
     TOY1 = """
@@ -76,4 +90,5 @@ if __name__ == "__main__":
     # sb = SBMLBuilder(level = 3, version = 1)
     sb = SBMLBuilder()
     toy1sbml =  sb.build_model(reducedPD.tree)
+    sb.save_SBML(toy1sbml, os.path.expanduser("~/Desktop/testsbml.sbml"))
     print("\n" + writeSBMLToString(toy1sbml) + "\n")
